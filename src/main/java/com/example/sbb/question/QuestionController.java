dc5f2f4acad0acf7bc9b2842ae3ee19754ec9bd2
HUISU SEO <juana7979@gmail.com>
2024-05-23 23:46:45 +0900

    테스트 데이터 생성 및 페이징 기능 추가

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -14,6 +14,8 @@ import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 import com.example.sbb.answer.AnswerForm;
 
+import org.springframework.data.domain.Page;
+
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -24,9 +26,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList",questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -12,6 +12,14 @@ import com.example.sbb.DataNotFoundException;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
@@ -37,5 +45,12 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10,Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 }
